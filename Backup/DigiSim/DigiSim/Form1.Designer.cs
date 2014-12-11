ecc988cf457211970d41205bfcdb7ae96641831a
Nico Kotlenga <NicoKotlenga@live.de>
2014-12-11 19:49:09 +0100

    Einrichtung
    Einrichtung der Github-Repository

---
+++ Backup/DigiSim/DigiSim/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace DigiSim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dateiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bearbeitenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hilfeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.speichernToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ladenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.beendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rückgängiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wiederholenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.überToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.funktionsumfangToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.gatterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.schalterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.löschenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bewegenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dateiToolStripMenuItem,
+            this.bearbeitenToolStripMenuItem,
+            this.hilfeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1902, 43);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dateiToolStripMenuItem
+            // 
+            this.dateiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.speichernToolStripMenuItem,
+            this.ladenToolStripMenuItem,
+            this.beendenToolStripMenuItem});
+            this.dateiToolStripMenuItem.Name = "dateiToolStripMenuItem";
+            this.dateiToolStripMenuItem.Size = new System.Drawing.Size(85, 39);
+            this.dateiToolStripMenuItem.Text = "Datei";
+            // 
+            // bearbeitenToolStripMenuItem
+            // 
+            this.bearbeitenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rückgängiToolStripMenuItem,
+            this.wiederholenToolStripMenuItem});
+            this.bearbeitenToolStripMenuItem.Name = "bearbeitenToolStripMenuItem";
+            this.bearbeitenToolStripMenuItem.Size = new System.Drawing.Size(145, 39);
+            this.bearbeitenToolStripMenuItem.Text = "Bearbeiten";
+            // 
+            // hilfeToolStripMenuItem
+            // 
+            this.hilfeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.überToolStripMenuItem,
+            this.funktionsumfangToolStripMenuItem});
+            this.hilfeToolStripMenuItem.Name = "hilfeToolStripMenuItem";
+            this.hilfeToolStripMenuItem.Size = new System.Drawing.Size(78, 39);
+            this.hilfeToolStripMenuItem.Text = "Hilfe";
+            // 
+            // speichernToolStripMenuItem
+            // 
+            this.speichernToolStripMenuItem.Name = "speichernToolStripMenuItem";
+            this.speichernToolStripMenuItem.Size = new System.Drawing.Size(199, 40);
+            this.speichernToolStripMenuItem.Text = "Speichern";
+            // 
+            // ladenToolStripMenuItem
+            // 
+            this.ladenToolStripMenuItem.Name = "ladenToolStripMenuItem";
+            this.ladenToolStripMenuItem.Size = new System.Drawing.Size(199, 40);
+            this.ladenToolStripMenuItem.Text = "Laden";
+            // 
+            // beendenToolStripMenuItem
+            // 
+            this.beendenToolStripMenuItem.Name = "beendenToolStripMenuItem";
+            this.beendenToolStripMenuItem.Size = new System.Drawing.Size(199, 40);
+            this.beendenToolStripMenuItem.Text = "Beenden";
+            // 
+            // rückgängiToolStripMenuItem
+            // 
+            this.rückgängiToolStripMenuItem.Name = "rückgängiToolStripMenuItem";
+            this.rückgängiToolStripMenuItem.Size = new System.Drawing.Size(231, 40);
+            this.rückgängiToolStripMenuItem.Text = "Rückgängig";
+            // 
+            // wiederholenToolStripMenuItem
+            // 
+            this.wiederholenToolStripMenuItem.Name = "wiederholenToolStripMenuItem";
+            this.wiederholenToolStripMenuItem.Size = new System.Drawing.Size(231, 40);
+            this.wiederholenToolStripMenuItem.Text = "Wiederholen";
+            // 
+            // überToolStripMenuItem
+            // 
+            this.überToolStripMenuItem.Name = "überToolStripMenuItem";
+            this.überToolStripMenuItem.Size = new System.Drawing.Size(282, 40);
+            this.überToolStripMenuItem.Text = "Über";
+            // 
+            // funktionsumfangToolStripMenuItem
+            // 
+            this.funktionsumfangToolStripMenuItem.Name = "funktionsumfangToolStripMenuItem";
+            this.funktionsumfangToolStripMenuItem.Size = new System.Drawing.Size(282, 40);
+            this.funktionsumfangToolStripMenuItem.Text = "Funktionsumfang";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gatterToolStripMenuItem,
+            this.schalterToolStripMenuItem,
+            this.löschenToolStripMenuItem,
+            this.bewegenToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(191, 164);
+            // 
+            // gatterToolStripMenuItem
+            // 
+            this.gatterToolStripMenuItem.Name = "gatterToolStripMenuItem";
+            this.gatterToolStripMenuItem.Size = new System.Drawing.Size(190, 40);
+            this.gatterToolStripMenuItem.Text = "Gatter";
+            // 
+            // schalterToolStripMenuItem
+            // 
+            this.schalterToolStripMenuItem.Name = "schalterToolStripMenuItem";
+            this.schalterToolStripMenuItem.Size = new System.Drawing.Size(190, 40);
+            this.schalterToolStripMenuItem.Text = "Schalter";
+            // 
+            // löschenToolStripMenuItem
+            // 
+            this.löschenToolStripMenuItem.Name = "löschenToolStripMenuItem";
+            this.löschenToolStripMenuItem.Size = new System.Drawing.Size(190, 40);
+            this.löschenToolStripMenuItem.Text = "Löschen";
+            // 
+            // bewegenToolStripMenuItem
+            // 
+            this.bewegenToolStripMenuItem.Name = "bewegenToolStripMenuItem";
+            this.bewegenToolStripMenuItem.Size = new System.Drawing.Size(190, 40);
+            this.bewegenToolStripMenuItem.Text = "Bewegen";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1902, 752);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "DigiSim";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dateiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem speichernToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ladenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem beendenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bearbeitenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rückgängiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wiederholenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hilfeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem überToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem funktionsumfangToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gatterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem schalterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem löschenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bewegenToolStripMenuItem;
+    }
+}
+
---
+++ Backup/DigiSim/DigiSim/GateObject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DigiSim
+{
+    class GateObject
+    {
+        //Attribute
+        double xPosition;
+        double yPosition;
+        double lenght;
+        double width;
+        int inputs;
+        int outputs;
+        System.Collections.ArrayList allPins = new System.Collections.ArrayList();
+
+        //Konstruktor für die Initialiserung und Zeichnung des Obejktes
+        public GateObject(double xPosition, double yPosition)
+        {
+            this.xPosition = xPosition;
+            this.yPosition = yPosition;
+        }
+        //Methode um die aktuelle Position zu ändern
+        public void ChangePosition(double xPosition, double yPosition)
+        {
+            this.xPosition = xPosition;
+            this.yPosition = yPosition;
+        }
+        //Methode für die Änderung der Eingänge
+        public void ChangeInput(int Anzahl)
+        {
+            inputs = Anzahl;
+        }
+        //Zurückgeben der eignen Pins
+        public System.Collections.ArrayList getPins()
+        {
+            return allPins;
+        }
+    }
+}
